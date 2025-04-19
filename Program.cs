236b929a3d8e8ebeab0ed8d9bcb257f9fa91c706
7H3LaughingMan <7H3LaughingMan@proton.me>
2025-04-18 20:16:54 -0500

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+using Velopack;
+using Velopack.Locators;
+
+namespace KCD2;
+
+partial class Program
+{
+    private static readonly NuGet.Versioning.SemanticVersion CurrentVersion = new(1, 0, 0);
+
+    private static readonly VelopackLocator velopackLocator = VelopackLocator.GetDefault(null);
+
+    [GeneratedRegex(@"<UsedMods>([\S\s]*)<\/UsedMods>")]
+    private static partial Regex UsedMods();
+
+    static void Main(string[] args)
+    {
+        VelopackApp
+            .Build()
+            .SetLocator(velopackLocator)
+            .WithFirstRun(
+                (hook) =>
+                {
+                    Environment.Exit(0);
+                }
+            );
+
+        Console.WriteLine($"KCD2 Modlist Cleaner v{CurrentVersion}");
+        Console.WriteLine();
+
+        if (args.Length == 1)
+            ProccessFile(args[0]);
+
+        Console.Write("Press any key to continue . . . ");
+        Console.ReadKey();
+    }
+
+    static void ProccessFile(string path)
+    {
+        if (!File.Exists(path) || Path.GetExtension(path) != ".whs")
+        {
+            Console.WriteLine("You must provide a KCD2 save file!");
+            Console.WriteLine();
+            return;
+        }
+
+        var fileInfo = new FileInfo(path);
+        var directoryInfo = fileInfo.Directory;
+
+        if (fileInfo.Length == 0)
+        {
+            Console.WriteLine("You appear to have provided a corrupted filed!");
+            Console.WriteLine("Please provide a file that is not 0 KB.");
+            return;
+        }
+
+        var backup = Path.Combine(directoryInfo.FullName, $"{fileInfo.Name}.bak");
+
+        if (File.Exists(backup))
+        {
+            Console.WriteLine("You already have a backup of your save, please delete this before trying again.");
+            Console.WriteLine(backup);
+            Console.WriteLine();
+            return;
+        }
+
+        fileInfo.CopyTo(backup);
+
+        var bytes = File.ReadAllBytes(fileInfo.FullName);
+        var fileHeader = BitConverter.ToUInt32(bytes, 0);
+
+        if (fileHeader != uint.MaxValue)
+        {
+            Console.WriteLine("The save file you provided does not have the proper signature!");
+            Console.WriteLine();
+            return;
+        }
+
+        var saveDescriptionLength = BitConverter.ToInt32(bytes, 4);
+        var saveDescription = Encoding.UTF8.GetString(bytes, 8, saveDescriptionLength);
+
+        var modifiedSaveDescription = UsedMods().Replace(saveDescription, "<UsedMods></UsedMods>");
+
+        using var fileStream = File.Open("exit.whs", FileMode.Create);
+
+        fileStream.Write(BitConverter.GetBytes(uint.MaxValue));
+        fileStream.Write(BitConverter.GetBytes(modifiedSaveDescription.Length));
+        fileStream.Write(Encoding.UTF8.GetBytes(modifiedSaveDescription));
+        fileStream.Write(bytes, 8 + saveDescriptionLength, bytes.Length - 8 - saveDescriptionLength);
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# KCD2 Modlist Cleaner
+
+This tool is designed to fix your Kingdom Come Deliverance II saves temporarily until Warhorse fixes the underlying issue.
+
+This is a re-implementation of the tool designed by [Catlord](https://next.nexusmods.com/profile/Catllord).
+
+## How To Use
+
+Once you have this tool installed all you need to do is right-click on the save file in question and select the "Clean Modlist" option. If you don't see this option you are using the new context menu and will have to hold shift when right-clicking.
